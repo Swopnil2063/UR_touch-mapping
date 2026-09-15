d0adf6ddd417d7b6a10646e1d7f47cb71a025bad
Swopnil Shrestha <swopnil.s1717@gmail.com>
2026-09-15 16:27:22 -0500

    Add ROS2 source setup script for Jetson

---
+++ setup_ros2_source.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list
